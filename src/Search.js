33dd0ae064ccd745b93c45a7917507aba2e90d67
Rob Brennan <rob@therobbrennan.com>
2020-04-07 20:36:09 -0700

    Crude implementation of Search

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,22 @@
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+
 export default () => {
+  const [searchText, setSearchText] = useState()
+  const { register, handleSubmit } = useForm()
+  const onSubmit = data => {
+    console.log(`onSubmit received data: ${JSON.stringify(data, null, 2)}`);
+    setSearchText(data.search)
+  }
+
   return (
     <>
       <h1>Search</h1>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input name='search' placeholder='Search...' ref={register} />
+        <input type='submit' />
+      </form>
+      {searchText && (<pre>{JSON.stringify(searchText, null, 2)}</pre>)}
     </>
   )
 }
